3cb50cb07f7b9015a60d70a1e8627e8a0b5ac054
gleclerc <gleclerc@iol.unh.edu>
2022-02-28 16:27:16 -0500

    update fill-memcahced to modern package

--- scripts/fill-memcached.py
+++ scripts/fill-memcached.py
@@ -1,10 +1,10 @@
-import memcache
+from pymemcache.client.base import Client
 import time
 import math
 
 print("filling memcache")
-servers = ["localhost:11211"]
-mc = memcache.Client(servers)
+servers = ("localhost", "11211")
+mc = Client(servers)
 rand = open("/dev/urandom", "rb")
 
 interval = 3600
