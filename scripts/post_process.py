6a6c9202d8a2242728a12a1d7d098384ddceba01
Griffin Leclerc <leclercgriffin@gmail.com>
2022-04-12 12:41:11 -0400

    master %

--- scripts/post_process.py
+++ scripts/post_process.py
@@ -1,17 +1,21 @@
 import statistics as stats
 
-file1 = open('res', 'r')
+file1 = open('../res', 'r')
 lines = file1.readlines()
 
 def mapInt(num):
     return int(num)
+
+def percent(num):
+    return (num * 100)
 
 def printThresh(l, threshold):
     outliers = list(filter(lambda n: n > threshold, l))
     threshCount = len(outliers)
     print("Values above " + str(threshold))
     print("     Number: " + str(threshCount))
-    print("     Ratio: " + str(threshCount) + "/" + str(count) + " | or " + str(threshCount/count))
+    percent = float('%.2f'%((threshCount/count) * 100))
+    print("     Ratio: " + str(threshCount) + "/" + str(count) + " | or " + percent + "%")
 
 
 lines = list(map(mapInt, lines))
