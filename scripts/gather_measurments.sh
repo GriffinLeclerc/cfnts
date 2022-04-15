6cf39b6eea648ca0516afbd8938ef48b440faf84
Griffin Leclerc <leclercgriffin@gmail.com>
2022-04-15 15:45:15 -0400

    master use dedicated low power server

--- scripts/gather_measurments.sh
+++ scripts/gather_measurments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# for i in {1..30000}
-# do
-#    ./target/release/cfnts client -c tests/intermediate.pem doctor.iol.unh.edu
-# done
+for i in {1..30000}
+do
+   ./target/release/cfnts client -c tests/intermediate.pem nts-server.iol.unh.edu
+done
 
-./target/release/cfnts client -c tests/intermediate.pem doctor.iol.unh.edu
+# ./target/release/cfnts client -c tests/intermediate.pem doctor.iol.unh.edu
