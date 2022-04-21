ca71e00f3d78f103cc54560e234f4d5e6bffd244
Griffin Leclerc <leclercgriffin@gmail.com>
2022-04-21 18:20:39 -0400

    master plot

--- scripts/plot.py
+++ scripts/plot.py
@@ -6,38 +6,55 @@ def mapInt(num):
     return int(num)
 
 def cullOutliers(data):
-    avg = stats.mean(data) * 10
+    avg = stats.mean(data)
     newData = []
-    
+
     for point in data:
-        if point < avg * 10:
+        if point < avg * 1000:
             newData.append(point)
 
     return newData
 
 def plot(filename, plotname):
     file1 = open(filename, 'r')
-    data = list(map(mapInt, file1.readlines()))
+    data = list(file1.readlines())
 
-    data = cullOutliers(data)
+    numClients = 0
+    measurements = []
 
+    plotClientNums = []
+    plotMeasurements = []
+
+    for line in data:
+        if line.strip() == "":
+            continue
+
+        if "client(s)" in line:
+            if numClients != 0:
+                # store this point
+                plotClientNums.append(numClients)
+                plotMeasurements.append(stats.median(measurements))
+            
+            # remember the next number of clients
+            numClients = int(line.replace(" client(s)\n", ""))
+        else:
+            measurements.append(int(line))
+
+    # print(plotClientNums)
+    # print(plotMeasurements)
+    
     plt.figure()
-
-    plt.xlabel = "Measurement Number"
-    plt.ylabel = "Total Operational Time"
 
     plt.gcf().set_size_inches(10, 5)
 
-    # plt.gca().yaxis.set_major_formatter(tick.FormatStrFormatter('ns'))
+    plt.set_xlabel = "Number of Clients"
+    plt.set_ylabel = "Total Operational Time"
 
-    measNums = list(range(0, len(data)))
-
-    plt.plot(measNums, data)
-
+    plt.plot(plotClientNums, plotMeasurements)
     plt.savefig("figures/" + plotname + ".pdf")
 
 plot('results/server_ntp_enc', "Server NTP Encryption")
 plot('results/server_ke_create', "Server NTP Cookie Creation")
 
-plot('results/client_nts_ke', "Client NTS NTP Total Time")
+plot('results/client_nts_ntp', "Client NTS NTP Total Time")
 plot('results/client_nts_ke', "Client NTS KE Total Time")
