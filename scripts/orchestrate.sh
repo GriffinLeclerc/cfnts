4c29b47d5908ff9466fc54de5f85bdbab4d5d533
Griffin Leclerc <leclercgriffin@gmail.com>
2022-04-28 17:44:15 -0400

    master loop

--- scripts/orchestrate.sh
+++ scripts/orchestrate.sh
@@ -1,41 +1,43 @@
 s=nts-server.iol.unh.edu
-
-while ! ping -c1 $s &>/dev/null
-        do echo "Waiting"
-done
-echo "Server Online"
-
-(
-    # start the KE server
-    ssh -t iol@$s 'bash' < ./scripts/run_ke_server.sh
-) &
-disown %1
-
-# allow keys to populate
-sleep 0.5
-
-(
-    # start the NTP server
-    ssh -t iol@$s sudo 'bash' < ./scripts/run_ntp_server.sh
-) &
-disown %1
-
-# allow ntp to start
-sleep 0.5
-
 c=132.177.116.19
 
-while ! ping -c1 $c &>/dev/null
-        do echo "Waiting"
+while true
+do
+    while ! ping -c1 $s &>/dev/null
+            do echo "Waiting"
+    done
+    echo "Server Online"
+
+    (
+        # start the KE server
+        ssh -t iol@$s 'bash' < ./scripts/run_ke_server.sh
+    ) &
+    disown %1
+
+    # allow keys to populate
+    sleep 0.5
+
+    (
+        # start the NTP server
+        ssh -t iol@$s sudo 'bash' < ./scripts/run_ntp_server.sh
+    ) &
+    disown %1
+
+    # allow ntp to start
+    sleep 0.5
+
+    while ! ping -c1 $c &>/dev/null
+            do echo "Waiting"
+    done
+    echo "Client Online"
+
+    # Gather measurements
+    ssh -t iol@$c 'bash' < ./scripts/gather_measurements.sh
+
+
+
+    # pkill ./target/release/cfnts
+
+    ssh -t iol@$s sudo reboot
+    ssh -t iol@$c sudo reboot
 done
-echo "Client Online"
-
-# Gather measurements
-ssh -t iol@$c 'bash' < ./scripts/gather_measurements.sh
-
-
-
-# pkill ./target/release/cfnts
-
-ssh -t iol@$s sudo reboot
-ssh -t iol@$c sudo reboot
