67838263b502de5eb067f663bc9e007e5c2dad2d
Griffin Leclerc <leclercgriffin@gmail.com>
2022-04-30 20:29:22 -0400

    master correct pathing

--- scripts/clear_results.sh
+++ scripts/clear_results.sh
@@ -4,4 +4,4 @@ printf "" > results/client_nts_ntp
 printf "" > results/server_ke_create
 printf "" > results/server_ntp_enc
 
-printf "" > tests/reqs_per_client
+printf "" > tests/reqs_per_second
--- scripts/gather_measurements.sh
+++ scripts/gather_measurements.sh
@@ -8,7 +8,7 @@ cd /home/iol/cfnts
 min_reqs=$(yq '.step_size' tests/experiment.yaml)
 
 # start with the minimum number of requests
-[ -s "tests/reqs_per_client" ] || printf $min_reqs > tests/reqs_per_client
+[ -s "tests/reqs_per_second" ] || printf $min_reqs > tests/reqs_per_second
 
 # Run the experiment
 ./target/release/cfnts client -c tests/intermediate.pem nts-server.iol.unh.edu
