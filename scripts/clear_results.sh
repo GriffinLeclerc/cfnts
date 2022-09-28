52735b916ee4ee47631d627edd071a80717425bb
gleclerc <leclercgriffin@gmail.com>
2022-09-28 17:31:13 -0400

    master reset state when results are cleared

--- scripts/clear_results.sh
+++ scripts/clear_results.sh
@@ -6,8 +6,17 @@ printf "" > results/server_ke_create
 printf "" > results/server_ntp_alone
 printf "" > results/server_nts_auth
 
-printf "" > tests/reqs_per_second
-printf "" > tests/num_clients
-printf "" > tests/num_aux_clients
+min_reqs=$(yq '.step_size' tests/experiment.yaml)
+min_clients=$(yq '.starting_num_clients' tests/experiment.yaml)
 
-ssh -t iol@132.177.116.25 "printf 0 > cfnts/tests/num_aux_clients"
+# start with the minimum number of requests
+[ -s "tests/reqs_per_second" ] || printf $min_reqs > tests/reqs_per_second
+
+# start with the minimum number of clients
+[ -s "tests/num_clients" ] || printf $min_clients > tests/num_clients
+
+# start with the minimum number of aux clients
+[ -s "tests/num_aux_clients" ] || printf "0" > tests/num_aux_clients
+
+# reset the orcestrator
+# ssh -t iol@132.177.116.25 "printf 0 > cfnts/tests/num_aux_clients"
